4252ff348ce01c6b4adf0b9b719b17d6e70488f9
Daniel Hardej <danielhardej@gmail.com>
2018-08-06 19:40:16 +0100

    no message

---
+++ 4. Principles of Software Design/Week 3/InterfaceAbstract/MarkovFour.java
@@ -0,0 +1,69 @@
+
+/**
+ * Write a description of MarkovFour here.
+ * 
+ * @author Daniel J Hardej
+ * @version v1.0
+ */
+
+import java.util.*;
+
+public class MarkovFour implements IMarkovModel {
+    private String myText;
+    private Random myRandom;
+    
+    public MarkovFour() {
+        myRandom = new Random();
+    }
+    
+    public void setRandom(int seed){
+        myRandom = new Random(seed);
+    }
+    
+    public void setTraining(String s){
+        myText = s.trim();
+    }
+    
+    public String getRandomText(int numChars){
+        if (myText == null){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length()-4);
+        String key = myText.substring(index, index+4);
+        sb.append(key);
+        
+        for(int k=0; k < numChars-4; k++){
+            ArrayList<String> follows = getFollows(key);
+            if (follows.size() == 0) {
+                break;
+            }
+            index = myRandom.nextInt(follows.size());
+            String next = follows.get(index);
+            sb.append(next);
+            key = key.substring(1) + next;
+        }
+        
+        return sb.toString();
+    }
+    
+    public ArrayList<String> getFollows (String key) {
+        ArrayList<String> follows = new ArrayList<String>();
+        int pos = 0;
+        
+        while (pos < myText.length()) {
+            int start = myText.indexOf(key, pos);
+            if (start == -1) {
+                break;
+            }
+            if (start + key.length() > myText.length()-1) {
+                break;
+            }
+            String next = myText.substring(start+key.length(), start+key.length()+1);
+            follows.add(next);
+            pos = start + key.length();
+        }
+        
+        return follows;
+    }
+}
